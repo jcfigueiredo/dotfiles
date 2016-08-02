3d74fad490f7d364bdffe63bc3634860c513f338
jcfigueiredo <jcfigueiredo@gmail.com>
2016-08-02 10:25:58 -0400

    latest

--- python/completion.zsh
+++ python/completion.zsh
@@ -1,2 +1,14 @@
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+
+# pip zsh completion start
+function _pip_completion {
+  local words cword
+  read -Ac words
+  read -cn cword
+  reply=( $( COMP_WORDS="$words[*]" \
+             COMP_CWORD=$(( cword-1 )) \
+             PIP_AUTO_COMPLETE=1 $words[1] ) )
+}
+compctl -K _pip_completion pip
+# pip zsh completion end
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -5,8 +5,10 @@ if $(gls &>/dev/null)
 then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
-  alias ll="gls -l --color"
+  alias ll="gls -lFh --color"
   alias la='gls -A --color'
 fi
 
 alias http-server='python -m SimpleHTTPServer'
+#alias weather="curl -s ipinfo.io/$1 | curl -s wttr.in/$1 | head -n 38"
+alias weather="curl -s wttr.in/72.229.28.185 | head -n 38"
